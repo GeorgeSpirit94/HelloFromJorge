0df280993c766fbacd3d1bf459e6238a648bc44d
Everton543 <64697974+Everton543@users.noreply.github.com>
2020-10-10 10:11:50 -0300

    Put my grettings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jorge");
+        System.out.println("Hello, here is Everton");
     }
 }
