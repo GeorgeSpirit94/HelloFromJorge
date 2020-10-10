7a07699016bdede4620d1b44a920614d2c43974e
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2020-10-10 10:40:21 -0700

    I put my name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jorge");
-        System.out.println("Hello Jorge, Cheri, Jaden, my is Everton");
+        System.out.println("Hello Jorge, Cheri, Jaden, my name is Everton");
+        System.out.println("Hello Jorge, Jaden, Everton, my name is Cheri");
+        
     }
 }
